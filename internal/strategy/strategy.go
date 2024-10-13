5d3e0cbbd07f34a5a71725a2c63fb71265da793a
nithish ravindra <nithishravindra8@gmail.com>
2024-10-13 21:34:54 +0530

    add: roundrobin strategy

--- internal/strategy/strategy.go
+++ internal/strategy/strategy.go
@@ -1,1 +1,17 @@
 package strategy
+
+import "sync"
+
+type RoundRobin struct {
+	Mu     sync.Mutex
+	CurAdd int
+}
+
+func (r *RoundRobin) RoundRobin(address []string) string {
+	r.Mu.Lock()
+	selectedAddress := address[r.CurAdd]
+	r.CurAdd = (r.CurAdd + 1) % len(address)
+	r.Mu.Unlock()
+
+	return selectedAddress
+}
--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net"
+
+	"github.com/nithishravindra/byo-lb-l4/internal/strategy"
 )
 
 func handleConnection(clientConn net.Conn, backendAddress string) {
 	defer clientConn.Close()
 
-	// Dial the backend server
 	backendConn, err := net.Dial("tcp", backendAddress)
+
 	if err != nil {
 		log.Println("Error connecting to backend server:", err)
 		return
 	}
 	defer backendConn.Close()
-
-	fmt.Println("Connected to backend server at", backendAddress)
-
-	// Channel to signal when either direction is done
 	done := make(chan struct{})
 
 	// Forward data from client to backend
@@ -39,20 +36,23 @@ func handleConnection(clientConn net.Conn, backendAddress string) {
 	<-done
 }
 
-func main() {
-	log.Println("Initialized load balancer")
-	address := "localhost:9090"
-	backendAddress := "localhost:8080"
-
-	// Start listening on the specified address
-	listener, err := net.Listen("tcp", address)
+func getTcpConnection(req string) net.Listener {
+	listener, err := net.Listen("tcp", req)
 	if err != nil {
 		log.Panic("Error creating TCP server:", err)
 	}
+	// Do not close the listener here
+	return listener
+}
+
+func main() {
+	log.Println("Initialized load balancer")
+	backendAddressList := []string{"localhost:8080", "localhost:8081", "localhost:8082", "localhost:8083"}
+
+	address := "localhost:9090"
+	listener := getTcpConnection(address)
 	defer listener.Close()
-
-	fmt.Printf("Listening on address: %v, forwarding to backend: %v\n", address, backendAddress)
-
+	rr := &strategy.RoundRobin{}
 	// Accept incoming connections
 	for {
 		clientConn, err := listener.Accept()
@@ -61,6 +61,8 @@ func main() {
 			continue
 		}
 
+		backendAddress := rr.RoundRobin(backendAddressList)
+		log.Printf("Listening on address: %v, forwarding to backend %v\n", address, backendAddress)
 		// Handle the connection in a separate goroutine
 		go handleConnection(clientConn, backendAddress)
 	}
